f85d1c9b7bb4281ac68cd8be0f17634349d12b28
Fabian Vergara <vergarf@gmail.com>
2018-10-06 17:57:33 -0400

    Implemented movements on gameboard

---
+++ GameController.py
@@ -0,0 +1,6 @@
+from Models import *
+
+input = raw_input('Enter your input:')
+MyBoard = GameBoard(input)
+
+print MyBoard
--- Models.py
+++ Models.py
@@ -1,1 +1,123 @@
 ## we will store all our model classes here so we can import them and use them lately in our controller
+from enum import Enum
+
+class Movement(Enum):
+    UP = 1
+    UP_RIGHT = 2
+    RIGHT = 3
+    DOWN_RIGHT = 4
+    DOWN = 5
+    DOWN_LEFT =6
+    LEFT = 7
+    UP_LEFT = 8
+
+class GameBoard():
+    Board = []
+    BoardPosition = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l"}
+    CurrentPosition = 0
+    CurrentPositionByLetter = "-"
+
+    def __init__(self, numbers):
+        ##TODO : Remove magic number
+        if len(str(numbers).strip()) != 26:
+           raise TypeError("Error trying to build the gameboard. You did not submit an invalid input")
+        else:
+            self.Board = [ x for x in numbers.split(" ") if str(x).isdigit()]
+            self.findCurrentPosition()
+
+    def __str__(self):
+        return str(self.Board)
+
+    def findCurrentPosition(self):
+        for x in range(len(self.Board)):
+            if (self.Board[x] == 0):
+                self.CurrentPosition = x
+                self.CurrentPositionByLetter = self.BoardPosition[x]
+
+##TODO : Refactor code (remove redundant code)
+    def move(self, movement):
+        if not isinstance(movement, Movement):
+            raise TypeError("movement must be an instance of Movement enum (a valid movement)")
+        else:
+            if (movement is  Movement.RIGHT):
+                self.MoveRight()
+
+            if (movement is Movement.LEFT):
+                self.MoveLeft()
+
+            if (movement is Movement.DOWN):
+                self.MoveDown()
+
+            if (movement is Movement.UP):
+                self.MoveUp()
+
+            if (movement is Movement.DOWN_LEFT):
+                try:
+                    self.MoveLeft()
+                    self.MoveDown()
+                except:
+                    print "Illegal move down left"
+
+            if (movement is Movement.DOWN_RIGHT):
+                try:
+                   self.MoveRight()
+                   self.MoveDown()
+                except:
+                    print "Illegal move down right"
+
+            if (movement is Movement.UP_LEFT):
+                try:
+                    self.MoveLeft()
+                    self.MoveUp()
+                except:
+                    print "Illegal move up left"
+
+            if (movement is Movement.UP_RIGHT):
+                try:
+                    self.MoveRight()
+                    self.MoveUp()
+                except:
+                    print "Illegal move up right"
+
+
+
+    def MoveRight(self):
+        tempCurrentPos = self.CurrentPosition + 1
+        if (tempCurrentPos > len(self.Board)):
+            raise TypeError("Illegal movement")
+        elif (tempCurrentPos % 4 == 0):
+            raise TypeError("Illegal movement")
+        else:
+            self.CurrentPosition = tempCurrentPos
+            self.CurrentPositionByLetter = self.BoardPosition[self.CurrentPosition]
+
+    def MoveLeft(self):
+        tempCurrentPos = self.CurrentPosition - 1
+        if (tempCurrentPos < 0):
+            raise TypeError("Illegal movement")
+    ##TODO: May give problems in the future, check
+        elif (self.CurrentPosition % 4 == 0):
+            raise TypeError("Illegal movement")
+        else:
+            self.CurrentPosition = tempCurrentPos
+            self.CurrentPositionByLetter = self.BoardPosition[self.CurrentPosition]
+
+    def MoveDown(self):
+        tempCurrentPos = self.CurrentPosition + 4
+        if (tempCurrentPos > len(self.Board)):
+            raise TypeError("Illegal movement")
+        else:
+            self.CurrentPosition = tempCurrentPos
+            self.CurrentPositionByLetter = self.BoardPosition[self.CurrentPosition]
+
+    def MoveUp(self):
+        tempCurrentPos = self.CurrentPosition - 4
+        if (tempCurrentPos < 0):
+            raise TypeError("Illegal movement")
+        ##TODO: May give problems in the future, check
+        else:
+            self.CurrentPosition = tempCurrentPos
+            self.CurrentPositionByLetter = self.BoardPosition[self.CurrentPosition]
+
+
+
